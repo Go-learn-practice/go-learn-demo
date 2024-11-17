ea8d3ec3b0b123f286d25a85a3fd996ff5c7f6d4
xiongguofang <guofang@gmail.com>
2024-11-17 17:44:39 +0800

    jwt learn

---
+++ jwt/jwtV1/HS.go
@@ -0,0 +1,44 @@
+package jwtV1
+
+import "github.com/golang-jwt/jwt/v5"
+
+type HS struct {
+	Key        string
+	SignMethod HSSignMethod
+}
+
+type HSSignMethod string
+
+const (
+	HS256 = "HS256"
+	HS384 = "HS384"
+	HS512 = "HS512"
+)
+
+func (hs *HS) getHSSignMethod(name HSSignMethod) *jwt.SigningMethodHMAC {
+	switch name {
+	case HS256:
+		return jwt.SigningMethodHS256
+	case HS384:
+		return jwt.SigningMethodHS384
+	case HS512:
+		return jwt.SigningMethodHS512
+	}
+	return jwt.SigningMethodHS256
+}
+
+func (hs *HS) Encode(claims jwt.Claims) (string, error) {
+	token := jwt.NewWithClaims(hs.getHSSignMethod(hs.SignMethod), claims)
+	sign, err := token.SignedString([]byte(hs.Key))
+	if err != nil {
+		return "", err
+	}
+	return sign, nil
+}
+
+func (hs *HS) Decode(sign string, claims jwt.Claims) error {
+	_, err := jwt.ParseWithClaims(sign, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(hs.Key), nil
+	})
+	return err
+}
--- jwt/main.go
+++ jwt/main.go
@@ -1,3 +1,48 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"jwt/jwtV1"
+	"time"
+)
+
+/*
+1. 选择一种加密方式
+2. 构造payload部分
+3. 执行加密过程
+4. 返回 jwt 令牌
+*/
+
+type MyClaims struct {
+	Name   string `json:"name"`
+	Gender string `json:"gender"`
+	Age    int    `json:"age"`
+	jwt.RegisteredClaims
+}
+
+func main() {
+	myClaims := MyClaims{
+		Name:   "nick",
+		Gender: "male",
+		Age:    20,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	var outClaims MyClaims
+	var sign string
+	var err error
+
+	hs := jwtV1.HS{
+		Key: "your-256-bit-secret",
+	}
+	sign, err = hs.Encode(myClaims)
+	fmt.Println(sign, err)
+
+	err = hs.Decode(sign, &outClaims)
+	fmt.Println(err, outClaims)
+}
